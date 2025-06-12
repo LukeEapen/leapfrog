0fe58b42bccdf550c4ed6281dee0f6db04066b6e
Lallu <Luke.Lallu@BAIN.com>
2025-06-12 09:48:38 -0400

    start sh ..

---
+++ start.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo "Installing dependencies..."
+pip install -r requirements.txt
+
+echo "Starting MCP..."
+uvicorn use_mcp_agent:app --port 4000
+
+echo "Starting app..."
+python  python new-prd-workflow-file-v2.py
